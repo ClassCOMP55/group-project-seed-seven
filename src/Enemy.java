618bf07d0de776deca84a83bced29cb0e5cb78de
Joshua Myers <j_myers8@u.pacific.edu>
2026-03-16 19:04:08 -0700

    Two things:
    Imported the ACM Graphics package to the Enemy class and initialized
    variables damage, speed, and appearance

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,5 +1,9 @@
+import acm.graphics.*;
 
 public abstract class Enemy extends Entity {
+	private int damage;
+	private float speed;
+	private GImage appearance;
 
 	public Enemy(float x, float y, int health) {
 		super(x, y, health);
