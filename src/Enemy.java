dcf4f628f5a23fc9b17249bef42faed05c9b1159
Joshua Myers <j_myers8@u.pacific.edu>
2026-03-17 18:13:04 -0700

    Three things:
    1) Created a subclass that explicitly accesses x, y, and health
    2) Created references to variables "damage" and "speed"
    3) Added a visual representation for enemy (file may be different from
    enemy.png)

--- src/Enemy.java
+++ src/Enemy.java
@@ -5,9 +5,14 @@ public abstract class Enemy extends Entity {
 	private float speed;
 	private GImage appearance;
 
-	public Enemy(float x, float y, int health) {
+	public Enemy(float x, float y, int health, int damage, float speed) {
 		super(x, y, health);
-		// TODO Auto-generated constructor stub
+		this.damage = damage;
+		this.speed = speed;
+		
+		// Visual representation for the GamePanel to draw
+		appearance = new GImage("enemy.png");
+		add(appearance);
 	}
 
 }
