9f6d02e1f6790f3c73f5253f303d13690775ee1a
ClarkFojas <c_fojas@u.pacific.edu>
2026-03-20 02:30:25 -0700

    Added weapon system and cleaned test code

--- src/Weapon.java
+++ src/Weapon.java
@@ -1,4 +1,53 @@
-
 public class Weapon {
 
+    private String name;
+    private int damage;
+
+    public Weapon(String type) {
+        switch (type.toLowerCase()) {
+            case "laser sword":
+                name = "Laser Sword";
+                damage = 25;
+                break;
+
+            case "laser gun":
+                name = "Laser Gun";
+                damage = 18;
+                break;
+
+            case "iron sword":
+                name = "Iron Sword";
+                damage = 20;
+                break;
+
+            case "bow and arrow":
+                name = "Bow and Arrow";
+                damage = 15;
+                break;
+
+            case "axe":
+                name = "Axe";
+                damage = 30;
+                break;
+
+            default:
+                name = "Iron Sword";
+                damage = 20;
+        }
+    }
+
+    public void attack(Enemy enemy) {
+        if (enemy == null) return;
+
+        enemy.takeDamage(damage);
+        System.out.println(name + " hit for " + damage + " damage");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
 }
