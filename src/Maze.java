0295b2f748e7301a33fe787c5c6f5648ee467f63
moses <moses@MosesPC>
2026-03-22 09:25:39 -0700

    Added variables for the maze elements

--- src/Maze.java
+++ src/Maze.java
@@ -1,4 +1,21 @@
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+import java.util.*;
 
-public class Maze {
-
+public abstract class Maze extends GraphicsProgram {
+	private static final int EASY = 1;
+	private static final int MEDIUM = 2;
+	private static final int HARD = 3;
+	
+	private int rows;
+	private int cols;
+	private int cellSize;
+	
+	private int grid[][];
+	
+	private final int DIRS[][] = {
+			{-1, 0}, {1, 0}, {0, -1}, {0, 1}
+	};
 }
+
