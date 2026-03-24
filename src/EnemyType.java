5064e570a2462e929c6f8276809b8d3c809e25bf
Joshua Myers <j_myers8@u.pacific.edu>
2026-03-23 18:03:37 -0700

    Chase logic test code removed
    Also imported the random module

--- src/EnemyType.java
+++ src/EnemyType.java
@@ -1,11 +1,12 @@
 public enum EnemyType {
-	SPIDER, SKELETON, ALIEN;
+	SPIDER, SKELETON, ALIEN, MUTANT;
 	
 	public String toString() {
 		switch(this) {
 		case SPIDER: return "spider";
 		case SKELETON: return "skeleton";
 		case ALIEN: return "alien";
+		case MUTANT: return "mutant";
 		}
 		return "n/a";
 	}
