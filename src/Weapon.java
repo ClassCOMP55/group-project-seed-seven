ee6aa79c4461a24e407bd4769a4f82e54b7a2f25
ClarkFojas <c_fojas@u.pacific.edu>
2026-03-25 11:24:01 -0700

    Added finished weapon system with cooldowns

--- src/Weapon.java
+++ src/Weapon.java
@@ -2,37 +2,46 @@ public class Weapon {
 
     private String name;
     private int damage;
+    private int cooldown;
 
     public Weapon(String type) {
         switch (type.toLowerCase()) {
+
             case "laser sword":
                 name = "Laser Sword";
                 damage = 25;
+                cooldown = 1;
                 break;
 
             case "laser gun":
                 name = "Laser Gun";
                 damage = 18;
+                cooldown = 2;
                 break;
 
             case "iron sword":
                 name = "Iron Sword";
                 damage = 20;
+                cooldown = 1;
                 break;
 
             case "bow and arrow":
                 name = "Bow and Arrow";
                 damage = 15;
+                cooldown = 2;
                 break;
 
             case "axe":
                 name = "Axe";
                 damage = 30;
+                cooldown = 3;
                 break;
 
             default:
                 name = "Iron Sword";
                 damage = 20;
+                cooldown = 1;
+                break;
         }
     }
 
@@ -49,5 +58,9 @@ public class Weapon {
 
     public int getDamage() {
         return damage;
+    }
+
+    public int getCooldown() {
+        return cooldown;
     }
 }
