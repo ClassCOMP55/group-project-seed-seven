90f15c4b517bce8c766fc1005ad7dfcb640dc560
Joshua Myers <j_myers8@u.pacific.edu>
2026-03-30 18:25:51 -0700

    Wrote the levelManager and update() functions
    nextLevel() will be defined.

--- src/LevelManager.java
+++ src/LevelManager.java
@@ -1,4 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 
+// Manages level progression, generates maze, and scales enemy difficulty
+// Logic is handled by Joshua and Clark
 public class LevelManager {
-
+	private int currentLevel;
+	private List<Enemy> enemies;
+	private Maze maze;
+	
+	public LevelManager() {
+		this.currentLevel = 1;
+		this.enemies = new ArrayList<>();
+		this.maze = new Maze();
+	}
+	
+	// Updates level state. If all enemies are defeated, move to the next level.
+	public void update(Game game) {
+		// Check when all enemies are defeated
+		if (enemies.isEmpty()) {
+			nextLevel(game);
+		}
+	}
 }
