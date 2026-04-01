8c4408abc4242a32f4e87c3442b8b63ea5f5a8d7
Joshua Myers <j_myers8@u.pacific.edu>
2026-04-01 13:26:10 -0700

    Started work on the Orb class

--- src/Orb.java
+++ src/Orb.java
@@ -1,4 +1,18 @@
+import acm.graphics.*;
+import java.awt.Color;
 
-public class Orb {
+/**
+ * Represents collectible objects dropped
+ * by enemies or found in the maze.
+ * Based on the System Design Report's UML and descriptions.
+ */
+public class Orb extends GCompound {
+	
+	private int value; // Point or EXP value of the orb
+	
+	// Default constructor as used in Enemy.java's handleDeath() method.
+	public Orb() {
+		this(10); // Default value
+	}
 
 }
